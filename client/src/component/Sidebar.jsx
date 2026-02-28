a1357d69f94cda8c5d08d4fa217bd72b78e691d9
Rohit <rohitgupta1938@gmail.com>
2026-03-01 00:27:43 +0530

    ai-ExamNote

--- client/src/component/Sidebar.jsx
+++ client/src/component/Sidebar.jsx
@@ -1,26 +1,79 @@
 import React from "react";
 
-function Sidebar({result}) {
-    if( !result || !result.questions.long || !result.questions.short || !result.subTopics){
-        return null;
-    }
+function Sidebar({ result }) {
+  if (
+    !result ||
+    !result.questions.long ||
+    !result.questions.short ||
+    !result.subTopics
+  ) {
+    return null;
+  }
   return (
     <div className=" bg-white rounded-2xl border border-gray-200 shadow-sm p-4 space-y-6 ">
       <div className="flex items-center gap-2">
         <span className="text-xl">📌</span>
-        <span className="font-bold text-lg text-indigo-600">Quick Exam View</span>
+        <span className="font-bold text-lg text-indigo-600">
+          Quick Exam View
+        </span>
       </div>
       <section>
-        <p className="text-sm font-semibold text-gray-700 mb-3">⭐ Sub topics (Priority Wise)</p>
-        {
-            Object.entries(result.subTopics).map(([star ,topics])=>(
-                <div key={star} className="mb-3 rounded-lg bg-gray-50 border border-gray-200 p-3">
-                    <p className="text-sm font-semibold text-yellow-600 mb-1">
-                        {star} Priority
-                    </p>
-                </div>
-            ))
-        }
+        <p className="text-sm font-semibold text-gray-700 mb-3">
+          ⭐ Sub topics (Priority Wise)
+        </p>
+        {Object.entries(result.subTopics).map(([star, topics]) => (
+          <div
+            key={star}
+            className="mb-3 rounded-lg bg-gray-50 border border-gray-200 p-3"
+          >
+            <p className="text-sm font-bold text-yellow-600 mb-1">
+              {star} Priority
+            </p>
+            <ul className="list-disc ml-4 text-sm text-gray-700 space-y-1">
+              {topics.map((topic, i) => (
+                <li key={i}> {topic}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </section>
+      <section className="rounded-lg bg-yellow-50 border border-yellow-200 p-3">
+        <p className="text-sm font-semibold text-gray-700 mb-1">
+          🔥 Exam Importance
+        </p>
+        <span className="text-yellow-700 font-bold text-sm">
+          {result.importance}
+        </span>
+
+        <p className="text-sm font-semibold text-gray-700 mb-3">
+          ❓ Important Questions
+        </p>
+        <div className="mb-4 rounded-lg  bg-indigo-50 border border-indigo-200 p-3">
+          <p className="text-sm font-medium text-indigo-700 mb-2">
+            Short Questions
+          </p>
+          <ul className="list-disc ml-4 text-sm text-gray-700 space-y-1">
+            {result.questions.short.map((t, i) => (
+              <li key={i}>{t}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="mb-4 rounded-lg  bg-purple-50 border border-purple-200 p-3">
+          <p className="text-sm font-medium text-purple-700 mb-2">
+            Long Questions
+          </p>
+          <ul className="list-disc ml-4 text-sm text-gray-700 space-y-1">
+            {result.questions.long.map((t, i) => (
+              <li key={i}>{t}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="mb-4 rounded-lg  bg-blue-50 border border-blue-200 p-3">
+          <p className="text-sm font-medium text-blue-700 mb-2">Diagram</p>
+          <ul className="list-disc ml-4 text-sm text-gray-700 space-y-1">
+            {result.questions.diagram}
+          </ul>
+        </div>
       </section>
     </div>
   );
